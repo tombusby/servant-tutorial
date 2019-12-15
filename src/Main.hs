d2659066697f8a4e13a058ab6de175e05258daf3
Tom Busby <tom@busby.ninja>
2019-12-15 16:48:59 +0100

    Add very minimal API that serves /users

---
+++ src/Main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Prelude ()
+import Prelude.Compat
+
+import Data.Aeson
+import Data.Time.Calendar
+import Network.Wai.Handler.Warp
+import GHC.Generics
+import Servant
+
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  , registration_date :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
+  , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
+  ]
+
+type UserAPI1 = "users" :> Get '[JSON] [User]
+
+server1 :: Server UserAPI1
+server1 = return users1
+
+userAPI :: Proxy UserAPI1
+userAPI = Proxy
+
+app1 :: Application
+app1 = serve userAPI server1
+
+main :: IO ()
+main = run 8081 app1
